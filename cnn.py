34d00b618e141ff2384104994a45cffe89272cc4
James Cadman <james_cadman@aol.co.uk>
2024-06-27 19:27:56 +0100

    added model training, predictions and evaluation

--- cnn.py
+++ cnn.py
@@ -4,9 +4,11 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 
-num_epochs = 1
+num_epochs = 4
 batch_size = 4
 learning_rate = 1e-4
+device = torch.device("cpu")
+MODEL_OUTPUT_PATH = './cnn.pth'
 
 transform = transforms.Compose(
     [
@@ -27,6 +29,7 @@ classes = train_data.classes
 class ConvNet(nn.Module):
 
     def __init__(self):
+        super(ConvNet, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
@@ -34,18 +37,59 @@ class ConvNet(nn.Module):
         self.fc2 = nn.Linear(in_features=120, out_features=84)
         self.fc3 = nn.Linear(in_features=84, out_features=10)
 
-    def forward(self):
+    def forward(self, x):
         x = F.relu(self.conv1(x))
         x = self.pool(x)
         x = F.relu(self.conv2(x))
+        x = self.pool(x)
         x = x.view(-1, 16*5*5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
+        x = self.fc3(x)
         return x
 
 
-model = ConvNet.to("cpu")
+model = ConvNet().to(device)
 
-criterion = nn.CrossEntropyLoss()
+loss_func = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# TRAIN MODEL
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # forward pass
+        outputs = model(images)
+        loss = loss_func(outputs, labels)
+
+        # back propagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if i % 1000 == 0:
+            print(f"Epoch: {epoch}, Step: {i}, Loss: {loss.item():.4f}")
+
+print("Finished")
+
+# save output model
+torch.save(model.state_dict(), MODEL_OUTPUT_PATH)
+
+model = ConvNet()
+model.load_state_dict(torch.load(MODEL_OUTPUT_PATH))
+
+# EVALUATE MODEL
+correct = 0
+incorrect = 0
+with torch.no_grad():
+    for i, (images, labels) in enumerate(test_loader):
+        outputs = model(images)
+
+        _, predictions = torch.max(outputs, 1)
+
+        correct += (predictions == labels).sum().item()
+        incorrect += (predictions != labels).sum().item()
+
+print(f"Correct predictions: {correct}, Incorrect predictions: {incorrect}")
