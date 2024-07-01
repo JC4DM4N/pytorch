c18cd9e6cce84f1c2806c1891ce38b5684294293
James Cadman <james_cadman@aol.co.uk>
2024-07-01 18:26:33 +0100

    added skeleton of transformer model

---
+++ transformer.py
@@ -0,0 +1,40 @@
+
+
+class Embedding:
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+
+class PositionalEncoding:
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+
+class SelfAttention:
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+
+class Encoder:
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+
+class Decoder:
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
