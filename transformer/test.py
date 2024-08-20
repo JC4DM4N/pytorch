c94e6f4162a7a338f4d07431ab88ee2b2a4d8641
James Cadman <james_cadman@aol.co.uk>
2024-08-20 14:47:10 +0100

    added testing script following text's path through transformer components

--- transformer/test.py
+++ transformer/test.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 
-from transformer import Transformer, Embedding, Encoder
+from transformer import Transformer, Embedding, Encoder, Decoder
 from tokenisers import SimpleTokeniser
 
 # load dataset from datasets
@@ -10,15 +10,14 @@ data = load_dataset("stanfordnlp/imdb")
 texts = [item["text"] for item in data["train"].to_list()[0: 10]]
 vocab = SimpleTokeniser(0, []).get_vocab(xs=texts)
 
-# configuration for transformer model
+# configuration parameters for transformer model
 src_vocab_size = len(vocab)
 tgt_vocab_size = len(vocab)
 embed_dim = 512
 max_len = 128
 num_layers = 2
 heads = 2
-output_dim = 128
-
+output_dim = 10
 print("-"*50)
 print("Model Configuration:")
 print(f"src_vocab_size: {src_vocab_size}")
@@ -31,7 +30,7 @@ print(f"output_dim: {output_dim}")
 
 # tokenise example text
 tokeniser = SimpleTokeniser(max_len, vocab)
-tokens = tokeniser(texts[0])
+tokens = tokeniser(texts[0]).view(1, max_len)
 
 # test generate embeddings
 print("-"*50)
@@ -39,6 +38,7 @@ print("testing Embedding class...")
 embedding = Embedding(src_vocab_size, embed_dim)
 embedded_tokens = embedding(tokens)
 print(f"Embedding shape: {embedded_tokens.shape}")
+print(embedded_tokens)
 
 print("-"*50)
 print("testing Encoder class...")
@@ -52,31 +52,35 @@ encoder = Encoder(
 )
 encoded_text = encoder(tokens)
 print(f"Encoding shape: {encoded_text.shape}")
+print(encoded_text)
 
-# model = Transformer(
-#     src_vocab_size=src_vocab_size,
-#     tgt_vocab_size=tgt_vocab_size,
-#     embed_dim=embed_dim,
-#     max_len=max_len,
-#     num_layers=num_layers,
-#     heads=heads,
-#     output_dim=output_dim
-# )
+print("-"*50)
+print("testing Decoder class...")
+decoder = Decoder(
+    vocab_size=tgt_vocab_size,
+    embed_dim=embed_dim,
+    max_len=max_len,
+    num_layers=num_layers,
+    heads=heads,
+    output_dim=output_dim
+)
+decoded_output = decoder(tokens, encoded_text)
+print(f"Decoded output shape: {decoded_output.shape}")
+print(decoded_output)
 
-# import torch
-# from torch import nn
-#
-#
-# criterion = nn.CrossEntropyLoss(ignore_index=0)
-# optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-#
-# model.train()
-#
-# for epoch in range(100):
-#     optimizer.zero_grad()
-#     output = model(src_data, tgt_data[:, :-1])
-#     loss = criterion(output.contiguous().view(-1, tgt_vocab_size), tgt_data[:, 1:].contiguous().view(-1))
-#     loss.backward()
-#     optimizer.step()
-#     print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
 
+print("-"*50)
+print("testing full Transformer class...")
+model = Transformer(
+    src_vocab_size=src_vocab_size,
+    tgt_vocab_size=tgt_vocab_size,
+    embed_dim=embed_dim,
+    max_len=max_len,
+    num_layers=num_layers,
+    heads=heads,
+    output_dim=output_dim
+)
+
+output_scores = model(tokens, tokens)
+print(f"Output shape: {output_scores.shape}")
+print(output_scores)
