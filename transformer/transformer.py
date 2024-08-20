24d2806162590b5c40b54e05042ed83abe7ea883
James Cadman <james_cadman@aol.co.uk>
2024-08-20 08:55:46 +0100

    added decoder mask and implemented Transformer class

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -50,9 +50,12 @@ class MultiHeadedAttention(nn.Module):
 
         self.out = nn.Linear(self.d_k*self.heads, self.d_k)
 
-    def forward(self, query, key, value):
+    def forward(self, query, key, value, mask=None):
         """
-        :param x: embedding matrix of shape (batch_size, max_len, embed_dim)
+        :param query: Q embedding matrix of shape (batch_size, max_len, embed_dim)
+        :param key: K embedding matrix of shape (batch_size, max_len, embed_dim)
+        :param value: V embedding matrix of shape (batch_size, max_len, embed_dim)
+        :param mask: triangular mask for use in the decoder
         :return:
         """
         batch_size = query.shape[0]
@@ -74,11 +77,18 @@ class MultiHeadedAttention(nn.Module):
         key = key.permute(batch_size, self.heads, max_len, self.d_k)
         value = value.permute(batch_size, self.heads, max_len, self.d_k)
 
-        # calculate self-attention
-        attention_scores = F.softmax(
-            torch.matmul(query, key.permute(0, 1, 3, 2))/math.sqrt(self.d_k)
-        )                                                   # (batch_size, heads, max_len, max_len)
-        context = torch.matmul(attention_scores, value)     # (batch_size, heads, max_len, d_k)
+        # calculate self-attention (batch_size, heads, max_len, max_len)
+        attention_scores = torch.matmul(query, key.permute(0, 1, 3, 2))/math.sqrt(self.d_k)
+
+        # Masking for the decoder
+        if mask is not None:
+            attention_scores = attention_scores.masked_fill(mask == 0, float("-1e20"))
+
+        # apply softmax
+        attention_scores = F.softmax(attention_scores)
+
+        # multiply by value to get context (batch_size, heads, max_len, d_k)
+        context = torch.matmul(attention_scores, value)
 
         # reshape back to (batch_size, max_len, embed_dim)
         context = context.permute(0, 2, 1, 3).contiguous().view(batch_size, max_len, embed_dim)
@@ -100,11 +110,11 @@ class TransformerBlock(nn.Module):
         self.norm2 = nn.LayerNorm(embed_dim)
         self.dropout2 = nn.Dropout(0.2)
 
-    def forward(self, x):
+    def forward(self, query, key, value, mask=None):
         # multi-head layer
-        attention_out = self.multi_head(x, x, x)
+        attention_out = self.multi_head(query, key, value, mask)
         dropout1_out = self.dropout1(attention_out)
-        attention_out_residual = dropout1_out + x  # residual connection for first layer
+        attention_out_residual = dropout1_out + query  # residual connection for first layer
         norm1_out = self.norm1(attention_out_residual)
         # fc layers
         fc1_out = self.dropout(F.Relu(self.fc1(norm1_out)))
@@ -137,11 +147,11 @@ class DecoderBlock(nn.Module):
         self.dropout = nn.Dropout(0.2)
         self.transformer_block = TransformerBlock(embed_dim, heads)
 
-    def forward(self, query, key, value):
-        attention_out = self.multi_head(query, key, value)
+    def forward(self, query, key, value, mask=None):
+        attention_out = self.multi_head(query, key, value, mask)
         attention_norm = self.norm1(attention_out)
-        query = self.dropout(attention_norm + query)
-        out = self.transformer_block(query, key, value)
+        query = self.dropout(attention_norm) + query  # residual connection
+        out = self.transformer_block(query, key, value, mask)
         return out
 
 
@@ -156,27 +166,63 @@ class Decoder(nn.Module):
                 for _ in range(num_layers)
             ]
         )
-        self.transformer_block = TransformerBlock(embed_dim, heads)
         self.fc = nn.Linear(embed_dim, output_dim)
         self.dropout = nn.Dropout(0.2)
         self.softmax = F.softmax()
 
-    def forward(self, x):
+    def forward(self, x, enc_out, mask=None):
         x = self.embedding(x) + self.positional_encoding(x)
         x = self.dropout(x)
         for layer in self.layers:
-            x = layer(x)
-        x = self.transformer_block(x)
+            x = layer(x, enc_out, enc_out, mask)
         x = self.softmax(self.fc(x))
         return x
 
 
 class Transformer(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, vocab_size: int, embed_dim: int, max_len: int, num_layers: int, heads: int, output_dim: int):
+        super(Transformer, self).__init__()
 
-    def forward(self):
-        pass
+        self.encoder = Encoder(vocab_size, embed_dim, max_len, num_layers, heads)
+        self.decoder = Decoder(vocab_size, embed_dim, max_len, num_layers, heads, output_dim)
+
+    def make_tgt_mask(self, tgt: torch.Tensor):
+        """
+        Create a target mask for use in the decoder, which will be a sparse lower triangular matrix
+            filled with ones.
+
+        Args:
+            tgt (torch.Tensor): Target sequence.
+
+        Returns:
+            tgt_mask (torch.Tensor): Target mask.
+        """
+        batch_size, tgt_len = tgt.shape
+        # returns the lower triangular part of matrix filled with ones
+        tgt_mask = torch.tril(torch.ones((tgt_len, tgt_len))).expand(
+            batch_size, 1, tgt_len, tgt_len
+        )
+        return tgt_mask
+
+    def decode(self, tgt: torch.Tensor, enc_out: torch.Tensor, tgt_mask: torch.Tensor):
+        return self.decoder(tgt, enc_out, tgt_mask)
+
+    def forward(self, src: torch.Tensor, tgt: torch.Tensor):
+        """
+        Forward pass of the Transformer model.
+
+        Args:
+            src (torch.Tensor): Input to the encoder.
+            tgt (torch.Tensor): Input to the decoder.
+
+        Returns:
+            outputs (torch.Tensor): Final vector representing probabilities of each target word.
+        """
+        enc_output = self.encoder(src)
+        tgt_mask = self.make_tgt_mask(tgt)
+        out = self.decode(tgt, enc_output, tgt_mask)
+        return out
+
 
 if __name__ == "__main__":
 
