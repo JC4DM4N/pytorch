cfb61b6b069232ae6a54a1586321794e04a174cb
James Cadman <james_cadman@aol.co.uk>
2024-08-21 15:09:43 +0100

    tranformer text classification training script fully implemented

--- transformer/train.py
+++ transformer/train.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -32,62 +33,118 @@ class BERTClassifier(nn.Module):
         return x
 
 
-# load dataset from datasets and take a sample
-data = load_dataset("stanfordnlp/imdb")
+def main():
+    # CONFIG - training
+    MODEL_NAME = "bert_classifier"          # prefix for model files
+    MODEL_OUTPUT_PATH = "bert_classifier"   # path of directory to save model files
+    num_epochs = 100                        # training epochs
+    # batch_size = 4                          # batch size
+    learning_rate = 1e-3                    # learning rate
+    save_model_every = 10                   # no. epochs to save model weights file
+    load_epoch = 0                          # file index to load
+    device = torch.device("cpu")
+    train_model = True                      # boolean to perform training
+    eval_model = True                       # boolean to perform evaluation
 
-train_df = pd.DataFrame(data["train"]).sample(1000).reset_index(drop=True)
-test_df = pd.DataFrame(data["test"]).sample(1000).reset_index(drop=True)
+    # CONFIG - parameters for transformer model
+    embed_dim = 256
+    max_len = 128
+    num_layers = 2
+    heads = 2
+    output_dim = 1
+    threshold = 0.5                         # probability threshold for assigning labels
 
-# configuration parameters for transformer model
-embed_dim = 256
-max_len = 128
-num_layers = 2
-heads = 2
-output_dim = 1
+    # load dataset from datasets and take a sample
+    data = load_dataset("stanfordnlp/imdb")
 
-# train tokeniser using vocabulary in the train set only
-tokeniser = SimpleTokeniser(max_len, train_df["text"])
+    train_df = pd.DataFrame(data["train"]).sample(1000, random_state=1)
+    test_df = pd.DataFrame(data["test"]).sample(1000, random_state=1)
 
-vocab = tokeniser.vocab
-vocab_size = len(vocab)
-print("-"*50)
-print("Model Configuration:")
-print(f"vocab_size: {vocab_size}")
-print(f"embed_dim: {embed_dim}")
-print(f"max_len: {max_len}")
-print(f"num_layers: {num_layers}")
-print(f"heads: {heads}")
-print(f"output_dim: {output_dim}")
+    # train tokeniser using vocabulary in the train set only
+    tokeniser = SimpleTokeniser(max_len, train_df["text"])
 
-train_tokens = train_df["text"].apply(tokeniser)
-train_x = torch.stack(train_tokens.to_list())
-train_y = torch.tensor(train_df["label"].astype(float).to_list()).view(train_df.shape[0], 1)
+    vocab = tokeniser.vocab
+    vocab_size = len(vocab)
+    print("-"*50)
+    print("Model Configuration:")
+    print(f"vocab_size: {vocab_size}")
+    print(f"embed_dim: {embed_dim}")
+    print(f"max_len: {max_len}")
+    print(f"num_layers: {num_layers}")
+    print(f"heads: {heads}")
+    print(f"output_dim: {output_dim}")
+
+    train_tokens = train_df["text"].apply(tokeniser)
+    train_x = torch.stack(train_tokens.to_list())
+    train_y = torch.tensor(train_df["label"].astype(float).to_list()).view(train_df.shape[0], 1)
+
+    test_tokens = test_df["text"].apply(tokeniser)
+    test_x = torch.stack(test_tokens.to_list())
+    test_y = torch.tensor(test_df["label"].astype(float).to_list()).view(test_df.shape[0], 1)
+
+    if not os.path.exists(MODEL_OUTPUT_PATH):
+        os.mkdir(MODEL_OUTPUT_PATH)
+
+    # # test encoder class
+    print("-"*50)
+    print("initialising classifier...")
+    model = BERTClassifier(
+        vocab_size=vocab_size,
+        embed_dim=embed_dim,
+        max_len=max_len,
+        num_layers=num_layers,
+        heads=heads,
+        num_classes=output_dim
+    )
+
+    # load saved model
+    if load_epoch:
+        print("loading model weights...")
+        model.load_state_dict(torch.load(f"{MODEL_OUTPUT_PATH}/{MODEL_NAME}_epoch_{load_epoch}.pth"))
+    model = model.to(device)
+
+    if train_model:
+        print("-"*50)
+        print("training classifier...")
+        loss_func = nn.BCELoss()  # binary cross-entropy loss
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        for epoch in range(num_epochs):
+            optimizer.zero_grad()
+            preds = model(train_x)
+            loss = loss_func(preds, train_y)
+            loss.backward()
+            optimizer.step()
+            print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
+
+            if save_model_every and epoch % save_model_every == 0:
+                torch.save(model.state_dict(), f"{MODEL_OUTPUT_PATH}/{MODEL_NAME}_epoch_{epoch}.pth")
+
+        print("training complete...")
+        # save output model
+        torch.save(model.state_dict(), f"{MODEL_OUTPUT_PATH}/{MODEL_NAME}.pth")
+
+    if eval_model:
+        print("-"*50)
+        print("evaluating classifier... \n")
+
+        for split, texts, labels in (["train", train_x, train_y], ["test", test_x, test_y]):
+
+            probs = model(texts)
+            preds = (probs > threshold)*1
+
+            tp = ((preds == 1) & (labels == 1)).sum().item()
+            fp = ((preds == 1) & (labels == 0)).sum().item()
+            fn = ((preds == 0) & (labels == 1)).sum().item()
+            tn = ((preds == 0) & (labels == 0)).sum().item()
+
+            recall = tp/(tp+fn)
+            precision = tp/(tp+fp)
+            f1 = 0.5*(recall + precision)
+
+            print(f"Evalutation metrics on {split} set:")
+            print(f"TP: {tp}, FP: {fp}, FN: {fn}, TN: {tn}")
+            print(f"Recall: {recall}, Precision: {precision}, F1-Score: {f1} \n")
 
 
-test_tokens = test_df["text"].apply(tokeniser)
-test_x = torch.stack(test_tokens.to_list())
-test_y = torch.tensor(test_df["label"].astype(float).to_list()).view(test_df.shape[0], 1)
-
-# # test encoder class
-print("-"*50)
-print("initialising classifier...")
-model = BERTClassifier(
-    vocab_size=vocab_size,
-    embed_dim=embed_dim,
-    max_len=max_len,
-    num_layers=num_layers,
-    heads=heads,
-    num_classes=output_dim
-)
-
-print("-"*50)
-print("training classifier...")
-loss_func = nn.BCELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-for epoch in range(100):
-    optimizer.zero_grad()
-    preds = model(train_x)
-    loss = loss_func(preds, test_y)
-    loss.backward()
-    optimizer.step()
-    print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
+if __name__ == "__main__":
+    main()
